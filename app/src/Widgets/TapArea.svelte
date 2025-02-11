b6c6bea42117579ea4c231e16293efae252543af
turbokirichenko <turbokirichenko@outlook.com>
2025-02-11 02:09:17 +0200

    removed animation

--- app/src/Widgets/TapArea.svelte
+++ app/src/Widgets/TapArea.svelte
@@ -16,10 +16,13 @@
     left: 0;
     width: 100%;
     height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
-  .level-3,
-  .level-4,
-  .level-5 {
+
+  .level-disable {
     animation-name: area-rounded, area-colored, area-movement;
     animation-duration: 2s, 2s, 10s;
     animation-fill-mode: forwards, forwards;
@@ -29,11 +32,24 @@
     animation-delay: 1s, 1s, 3s;
   }
 
+  .level-non {
+    animation-name: area-move-and-scale;
+    animation-duration: 30s;
+    animation-timing-function: cubic-bezier(0.445, 0.05, 0.55, 0.95);
+    animation-iteration-count: infinite;
+    animation-delay: 3s;
+    opacity: 0;
+  }
+
   .level-0,
   .level-1,
   .level-2,
+  .level-3,
+  .level-4,
+  .level-5,
   .level-6 {
     width: 100%;
     height: 100%;
+    opacity: 0;
   }
 </style>
