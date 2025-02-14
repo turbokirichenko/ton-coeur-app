1ff1ffb6a83450dad400afea8ea57ff46c927f00
turbokirichenko <turbokirichenko@outlook.com>
2025-02-14 18:07:34 +0200

    added analytics

--- app/src/App/bootstrap.js
+++ app/src/App/bootstrap.js
@@ -1,7 +1,13 @@
 import { mount } from 'svelte'
 import App from './App.svelte'
+import telegramAnalytics from '@telegram-apps/analytics';
 
 export function bootstrap() {
+    //console.log(import.meta.env.VITE_ANALYTICS_TOKEN, import.meta.env.VITE_ANALYTICS_IDENTIFIER);
+    telegramAnalytics.init({
+        token: import.meta.env.VITE_ANALYTICS_TOKEN, // Token received via @DataChief_bot
+        appName: import.meta.env.VITE_ANALYTICS_IDENTIFIER, // The analytics identifier you entered in @DataChief_bot
+    });
     const app = mount(App, {
         target: document.getElementById('app'),
     })
