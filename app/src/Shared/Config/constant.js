4d4a8051c34fd0c01d599b753257c1879717d1b4
turbokirichenko <turbokirichenko@outlook.com>
2025-02-14 02:49:34 +0200

    updated url

--- app/src/Shared/Config/constant.js
+++ app/src/Shared/Config/constant.js
@@ -2,6 +2,6 @@ export const SHARE_LINK='https://turbokirichenko.github.io/ton-coeur-app/'
 export const SHARE_TEXT='A simple touch can create something magical. Just to click and gift ❤️'
 export const SHARE_TITLE='TonCœur Mini Telegram App'
 export const TG_SHARE_API='https://t.me/share/url?url='
-export const TG_APP_LINK='https://t.me/ton_coeur_dev_bot/toncouer'
+export const TG_APP_LINK='https://t.me/toncoeurbot/toncoeur'
 export const REPOSITORY_IMAGE='https://raw.githubusercontent.com/turbokirichenko/ton-coeur-app/refs/heads/main/app/public/snaps/'
 export const TG_ORIGIN='https://t.me/';
